1c134a4fbd688b75514faa870111fe4a0eac4f5a
Anton Knyazev <knyazevmac@icloud.com>
2024-09-14 14:05:25 +0300

    create home page

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from home.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
